f43bf1bcb69e40e509fee4ec13fd478a4253b34e
russell <russell.s.pierce@gmail.com>
2025-08-11 00:52:09 -0500

    We have to use ollama within a docker image

--- students/every_start.sh
+++ students/every_start.sh
@@ -1,12 +1,1 @@
 #!/bin/bash
-
-OLLAMA_COUNT=$(ps aux | awk '{  print $1 }' | grep 'ollama' | wc -l)
-
-if [ "$OLLAMA_COUNT" -ne 1 ]; then
-    if [ "$OLLAMA_COUNT" -gt 1 ]; then
-        pkill ollama || echo "Was unable to kill ollama"
-    fi
-    ollama serve || echo "Was unable to start ollama"
-else
-    echo "ollama is already running"
-fi

7a98d033e881a7f4f49912749ed27b9ad9ed0531
russell <russell.s.pierce@gmail.com>
2025-08-25 14:38:57 -0500

    fix alias definition

--- students/models.sh
+++ students/models.sh
@@ -1,4 +1,7 @@
-ollama > /dev/null 2>&1 || alias ollama='docker exec -it ollama ollama'
+if ! command -v ollama >/dev/null 2>&1; then
+  alias ollama='docker exec -it ollama ollama'
+fi
+alias ollama='docker exec -it ollama ollama'
 ollama pull gemma3n:latest
 ollama pull llama3.2:1b
 ollama pull gemma3:1b

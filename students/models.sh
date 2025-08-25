39a17c851df15c4637d536f73f09ccec81ac91ea
russell <russell.s.pierce@gmail.com>
2025-08-25 14:55:08 -0500

    alias not yet available

--- students/models.sh
+++ students/models.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+# aliases don't work in the same shell in which they are defined (maybe)
 if ! command -v ollama >/dev/null 2>&1; then
-  alias ollama='docker exec -it ollama ollama'
+  OLLAMA_CMD='docker exec -it ollama ollama'
+else
+  OLLAMA_CMD='ollama'
 fi
-alias ollama='docker exec -it ollama ollama'
-ollama pull gemma3n:latest
-ollama pull llama3.2:1b
-ollama pull gemma3:1b
-ollama pull gemma3n:e2b
+
+$OLLAMA_CMD pull gemma3n:latest
+$OLLAMA_CMD pull llama3.2:1b
+$OLLAMA_CMD pull gemma3:1b
+$OLLAMA_CMD pull gemma3n:e2b

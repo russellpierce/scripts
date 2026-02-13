6216e2dcb77d3103480e28afcfcfeb514ddc9139
Russell S. Pierce <Russell.s.pierce@gmail.com>
2026-02-12 22:39:17 -0600

    Get pyproject.toml if missing.
    Ensure requirements.txt is generated and dependencies are installed whether pyproject.toml is found or downloaded.

--- students/start_notebook_env.sh
+++ students/start_notebook_env.sh
@@ -6,12 +6,18 @@ if ! grep -qi 'amzn' /etc/os-release; then
 else
   uv --version || (curl -LsSf https://astral.sh/uv/install.sh | sh)
   echo "AWS" > "/tmp/runtime"
-  
+
   if [ -f pyproject.toml ]; then
     echo "pyproject.toml found. Generating requirements.txt and installing dependencies. This may take a few minutes..."
-    /home/ec2-user/.local/bin/uv pip compile pyproject.toml -o requirements.txt --quiet
-    pip install -q -r requirements.txt
+  else
+    echo "pyproject.toml not found. Downloading from https://raw.githubusercontent.com/russellpierce/scripts/refs/heads/main/students/pyproject.toml"
+    curl -O https://raw.githubusercontent.com/russellpierce/scripts/refs/heads/main/students/pyproject.toml
+    echo "pyproject.toml downloaded. Generating requirements.txt and installing dependencies. This may take a few minutes..."
   fi
+
+  echo "pyproject.toml found. Generating requirements.txt and installing dependencies. This may take a few minutes..."
+  /home/ec2-user/.local/bin/uv pip compile pyproject.toml -o requirements.txt --quiet
+  pip install -q -r requirements.txt
   
   if ! sudo docker ps --format '{{.Names}}' | grep -q '^ollama$'; then
     sudo docker run -d --name ollama -p 11434:11434 -v "$HOME/.ollama":/root/.ollama ollama/ollama:latest

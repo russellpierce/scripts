805fb62406f9890feb7bc0b367157dbc6d0ca67d
Russell S. Pierce <Russell.s.pierce@gmail.com>
2026-02-18 17:54:36 -0600

    Show when we enter models.sh
    Add echo statement to indicate script execution

--- students/models.sh
+++ students/models.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "in models.sh"
 # aliases don't work in the same shell in which they are defined (maybe)
 if ! command -v ollama >/dev/null 2>&1; then
   OLLAMA_CMD='docker exec -it ollama ollama'
